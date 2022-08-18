7331b719c07bd78bdae3a55bd300302450829d73
PALOMA AZEVEDO SANTOS <paloma.asantos17@senacsp.edu.br>
2022-08-17 21:58:06 -0300

    Adicionar arquivos de projeto.

---
+++ Prato.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaPratos
+{
+    public class Prato
+    {
+        public Prato() { }
+        public string NomePrato { get; set; }
+        public string Ingredientes { get; set; }
+        public string DescricaoPrato { get; set; }
+        public string UrlFoto { get; set; }
+        public string TempoPreparo { get; set; }
+
+        public Prato(string nomeprato, string ingredientes, string descricaoprato, string urlfoto, string tempopreparo)
+        {
+            this.NomePrato = nomeprato;
+            this.Ingredientes = ingredientes;
+            this.DescricaoPrato = descricaoprato;
+            this.UrlFoto = urlfoto;
+            this.TempoPreparo = tempopreparo;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaPratos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de Pratos");
+
+            List<Prato> pratos = new List<Prato>();
+
+            pratos.Add(new Prato("Macarronada", "Macarrão, sal, óleo, molho de tomate, carne moida e cebola", "Macarrão com molho", "https://img.cybercook.com.br/receitas/33/macarronada-4-840x480.jpeg?q=75", "30 minutos"));
+            pratos.Add(new Prato("Feijão", "Feijão cru, água, sal, óleo, alho", "Feijão fresco", "https://img.itdg.com.br/tdg/images/recipes/000/169/692/146782/146782_original.jpg?mode=crop&width=710&height=400", "1 hora"));
+            pratos.Add(new Prato("Pizza de Pepperoni", "Molho de tomate, água, orégano, cebola, sal, queijo parmesão, azeite, fermento, farinha de trigo, queijo mussarela, pepperoni", "Pizza de Pepperoni inspirada no Pizza Hut (Massa Pan)", "https://receitasdeminuto.com/wp-content/uploads/2019/11/pizza_pan_pizza_hut.jpg", "1 hora"));
+
+        }
+    }
+}
